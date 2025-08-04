31cd38c195ffec649360ef6680b81f02260bdb09
diego192599 <di.cramire@udeo.edu.gt>
2025-08-04 10:53:51 -0600

    creacion de funcion quick sort

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Ejercicio")
+def Nombre_estudiantes(lista):
+    if len(lista)<=1:
+        return lista
+    else:
+        pivote=lista[0]
+        menores=[x for x in lista[1:] if x.lower<=pivote.lower()]
+        mayores=[x for x in lista[1:] if x.lower>pivote.lower()]
+        return Nombre_estudiantes(menores)+pivote+Nombre_estudiantes(mayores)
+
